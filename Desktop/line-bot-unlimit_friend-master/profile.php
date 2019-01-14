ab7749ff820d0b49ff38f51ce3c5c35b2d2fe2e8
jurnpitak <46663577+jurnpitak@users.noreply.github.com>
2019-01-14 11:27:34 +0700

    update value

--- Desktop/line-bot-unlimit_friend-master/profile.php
+++ Desktop/line-bot-unlimit_friend-master/profile.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$access_token = '*Channel_Access_Token*';
+$access_token = 'sjgQeYbzuRD33xv2kF2gp0W+UOMs/KanzXg9p+Cv25rDBhkpmka1TMWPaaT56Z97J5DNxbo0qFCzDIVNjWw0kQn1zql38QtlAjBmA8itb7y8Bk1u2sGCWtGRoE6XyKTD/hI7weLZzUAu8evjs2kILgdB04t89/1O/w1cDnyilFU=';
 
-$userId = '*User_ID*';
+$userId = 'U9b2bccf7ac0215eeeac68b8469102646';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
